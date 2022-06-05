75ca1bb124e92690c8bb6c69fe4bc8a95b279721
nsara <kolia.sh12345@gmail.com>
2022-06-05 18:34:12 +0300

    fix pattern

--- Help/src/TEMPLATE_METHOD/AndisPizza.java
+++ Help/src/TEMPLATE_METHOD/AndisPizza.java
@@ -5,28 +5,33 @@ import FACADE.Order;
 public class AndisPizza extends DeliveryRestaurant{
 
     private String restaurantName;
+    private String UserLocation;
     private String restaurantLocation;
 
-    public AndisPizza(String restaurantName, String restaurantLocation) {
+    public AndisPizza(String restaurantName, String UserLocation) {
         this.restaurantName = restaurantName;
-        this.restaurantLocation = restaurantLocation;
+        this.UserLocation = UserLocation;
     }
 
     @Override
     public void changeOrderPrice(Order order) {
         int value = 20;
-        if(deliveryPossibility(this.restaurantLocation)){
-            order.setDiscount(-value);
-            order.setPrice(order.getPrice());
-            order.setKm(order.getKm() + value);
-        }
-        else{
+        String str;
+        restaurantLocation = super.randomRestaurantLocation();
+        if(restaurantLocation.equals(UserLocation)){
             order.setDiscount(value);
             order.setPrice(order.getPrice());
             order.setKm(Math.abs(order.getKm() - value));
+            str = "due to the fact that you are in the same city as the restaurant, the cost is reduced by ";
+        }
+        else{
+            order.setDiscount(-value);
+            order.setPrice(order.getPrice());
+            order.setKm(order.getKm() + value);
+            str = "due to the fact that you are in another city, the cost increases by ";
         }
         order.setCollectOrderInfo("\nRestaurant Location: " + restaurantLocation + "\nRestaurant Name: " + restaurantName +
-                "\ndue to the fact that you are in the same city as the restaurant, the cost is reduced by " + value);
+                "\n" + str + value);
         order.orderInfo();
     }
 }
